4e4b43fdbd21b71930bfee75937edbaca953398e
Nekuake - Alejandro Hurtado <ruizale55@gmail.com>
2018-05-28 11:03:17 +0200

    Tests and checks working. Files are not generated yet.

--- Main.py
+++ Main.py
@@ -1,52 +1,59 @@
 # Script done by Nekuake using Sivel's speedtest.
 import time
+import decimal
 
-# Tbe name of the variables used in this script
+timessofar = (0)
 times = 0
-timeout = 0
-timessofar = 0
+ping = 0
 download = 0
 upload = 0
-ping = 0
-kindoftest = 0
-testping = 0
-# Let's start
+
 print("This script needs that you have installed previously the speedtest package.\n")
 print("Please, be sure that you have it first.\n")
 try:
     import speedtest
+
     print("Package found! Ready to run!")
     s = speedtest.Speedtest()  # Used
 except ImportError as e:  # Package not found, scipt shouldn't be run
     print("Speedtest package hasn't been found. Closing...")
     input("Press any key...")
     exit()
-print("There shouldn't be any error, but if you have MemoryError, you should answer yes: \n")
+print("If you have MemoryError, you should answer yes. Otherwise, just press enter: \n")
 memorypreallocation = input("Disable memory preallocation? (press enter= no/ press Y = yes) >> ").upper()
-
-s.get_best_server()
 print("Set the values:")
-times = input("Number of times: >> ")
-timeout = input("How many seconds between every test: >> ")
-kindoftest = input("0 for upload and download speed, 1 for just download, 2 for just upload.")
-testping = input("Test ping? (0/1)")
-while timessofar != times:
-    if kindoftest == 0 or 1:
-        download=s.download()
-    if kindoftest == 1:
-        ping= s.lat_lon
-    if kindoftest == 0 or 2:
+times = int(input("Number of times: >> "))
+timeout = int(input("How many seconds between every test: >> "))
+kindoftestdown = int(input("Test download speed? (0/1)"))
+kindoftestup = int(input("Test upload speed? (0/1)"))
+testping = int(input("Test ping? (0/1)"))
+while timessofar < times:
+    if timessofar == 0:
+        s.get_best_server()
+    else:
+        time.sleep(timeout)
+    timessofar = timessofar + 1
+    if kindoftestdown == 1:
+        print("Testing download speed...(", timessofar, ")/(" , times, ")")
+        download = s.download()
+        download = download / 1024
+        download = round(download)
+        print("Download Speed (", timessofar, "): ", download, "KB/s. ", download/1024 , "MB/s")
+    if testping == 1:
+        print("Testing ping...(", timessofar, ")")
+        ping = s.lat_lon
+        print(ping)
+    if kindoftestup == 1:
+        print("Testing upload speed...(", timessofar, ")")
         if memorypreallocation == "Y":
             upload = s.upload(pre_allocate=False)
         elif memorypreallocation != "Y":
             upload = s.upload()
-        if ping != 0:
-            print(ping)
-        if download != 0:
-            print(download)
-        if upload != 0:
-            print(upload)
-    #with open("Result.txt", "w") as  text_file:
-     #   print= (s.download(), file = result.txt)
+        upload = upload / 1024
+        upload = round(upload)
+        print("Upload Speed (", timessofar, "): ", upload, "KB/s. ", upload / 1024, "MB/s")
+print("Schelude completed. Check the output file.")
+# with open("Result.txt", "w") as  text_file:
+#   print= (s.download(), file = result.txt)
 
-      #  time.sleep(timeout)
+#  time.sleep(timeout)
