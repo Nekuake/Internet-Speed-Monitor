b75e4f8c624cae1115ba7e920546f66e91cf4733
Nekuake - Alejandro Hurtado <ruizale55@gmail.com>
2018-06-19 10:41:10 +0200

    Branch created

--- Main-GUI.py
+++ Main-GUI.py
@@ -1,7 +1,8 @@
 # Script done by Nekuake using Sivel's speedtest.
 import time
+import threading
 from tkinter import *
-
+from tkinter.ttk import *
 try:
     import speedtest
 
@@ -11,9 +12,14 @@ try:
 except ImportError as e:  # Package not found, scipt shouldn't be run
     status = -1
 closed = 0
+download = 0
+upload = 0
+ping = 0
+timewaited = 0
+timessofar = 0
 mainwindow = Tk()
 mainwindow.tk.call('tk', 'scaling', 2.0)
-mainwindow.geometry('500x300')
+mainwindow.geometry('550x100')
 mainwindow.configure(bg="#F0F0F0")
 mainwindow.title("Internet Speed Monitor")
 
@@ -32,7 +38,6 @@ Checkbutton(mainwindow, text="Disable Mem. preallocation.", variable=memorypreal
 def test():
     global times, kindoftestping, kindoftestdown, kindoftestdown, kindoftestping, timeout, status, mainwindow
     print("Starting test function...")
-    mainwindow.withdraw()
     timesdef = int(times.get())
     timeoutdef = int(timeout.get())
     timessofar = 1
@@ -40,14 +45,6 @@ def test():
     kindoftestpingdef = kindoftestping.get()
     kindoftestdowndef = kindoftestdown.get()
     kindoftestupdef = kindoftestup.get()
-    resultwindow = Tk()
-    resultwindow.tk.call('tk', 'scaling', 2.0)
-    resultwindow.geometry('500x300')
-    resultwindow.configure(bg="#F0F0F0")
-    resultwindow.title("Internet Speed Monitor-Results")
-    Label(resultwindow, text="Results").grid(row=0, column=0, sticky=NW)
-
-    resultwindow.update()
     while timessofar <= timesdef:
         if closed == 1:
             mainwindow.deiconify()
@@ -65,7 +62,7 @@ def test():
                 kindoftestupdef) + str(
                 kindoftestdowndef) + str(kindoftestpingdef) + str(timeoutdef))
         else:
-            print("Waiting", timeout, " seconds...")
+            print("Waiting", str(timeout), " seconds...")
 
             while timewaited != timeoutdef:
                 time.sleep(1)
@@ -82,19 +79,13 @@ def test():
             f.write("\nDownload Speed (" + str(timessofar) + "): " + str(download) + "KB/s. " + str(
                 download / 1024) + "MB/s")
             download = str(download)
-            downloadgui = Label(resultwindow, text="0 MB/s")
-            downloadgui.grid(row=1, column=1, sticky=NW)
-            downloadgui.configure(text="Downloadgui Speed: " + download + " KB/s.")
-            resultwindow.update()
+
         if kindoftestpingdef == 1:
             ping = s.lat_lon
             f.write("\nPing:" + str(ping))
             print("\nPing:" + str(ping))
             ping = str(ping)
-            pinggui = Label(resultwindow, text="0 ms")
-            pinggui.grid(row=2, column=1, sticky=NW)
-            pinggui.configure(text="Ping: " + ping)
-            resultwindow.update()
+
         if kindoftestupdef == 1:
             if memorypreallocation == "Y":
                 upload = s.upload(pre_allocate=False)
@@ -105,12 +96,38 @@ def test():
             print("\nUpload Speed (" + str(timessofar) + "): " + str(upload) + "KB/s. " + str(upload / 1024) + "MB/s")
             f.write("\nUpload Speed (" + str(timessofar) + "): " + str(upload) + "KB/s. " + str(upload / 1024) + "MB/s")
             upload = str(upload)
-            uploadgui = Label(resultwindow, text="0 MB/s")
-            uploadgui.grid(row=3, column=1, sticky=NW)
-            uploadgui.configure(text="Upload Speed: " + upload + " KB/s.")
-            resultwindow.update()
+
         timessofar = timessofar + 1
+
+        print(round((timessofar/timesdef)*100))
         f.close
+        timewaited = 0
+
+
+def resultsgui():
+    global timesdef
+    resultwindow = Tk()
+    resultwindow.tk.call('tk', 'scaling', 2.0)
+    resultwindow.geometry('500x300')
+    resultwindow.configure(bg="#F0F0F0")
+    resultwindow.title("Internet Speed Monitor-Results")
+    globalprogressbar = Progressbar(resultwindow, orient=VERTICAL, length=100, mode='determinate', maximum=timesdef)
+    globalprogressbar.grid(row=4, column=2, sticky=SW)
+    Label(resultwindow, text="Results").grid(row=0, column=0, sticky=NW)
+    resultwindow.update()
+    downloadgui = Label(resultwindow, text="0 MB/s")
+    downloadgui.grid(row=1, column=1, sticky=NW)
+    downloadgui.configure(text="Downloadgui Speed: " + download + " KB/s.")
+    resultwindow.update()
+    uploadgui = Label(resultwindow, text="0 MB/s")
+    uploadgui.grid(row=3, column=1, sticky=NW)
+    uploadgui.configure(text="Upload Speed: " + upload + " KB/s.")
+    resultwindow.update()
+    globalprogressbar.step(1)
+    pinggui = Label(resultwindow, text="0 ms")
+    pinggui.grid(row=2, column=1, sticky=NW)
+    pinggui.configure(text="Ping: " + ping)
+    resultwindow.update()
 
 
 Label(mainwindow, text="Nº of tests").grid(row=4, column=0, sticky=NW)
@@ -119,10 +136,5 @@ times.grid(row=4, column=1, sticky=NW)
 Label(mainwindow, text="Seconds").grid(row=4, column=2, sticky=NW)
 timeout = Spinbox(mainwindow, width=10, from_=1, to=9999)
 timeout.grid(row=4, column=3, sticky=NW)
-if status == 0:
-    Label(mainwindow, text="OK. Waiting conf.", fg="green").grid(row=5, column=0)
-else:
-    Label(mainwindow, text="INSTALL THE SPEEDTEST PYTHON MODULE", fg="red").grid(row=5, column=0, sticky=NW)
-if status == 0:
-    Button(mainwindow, text="Ready to run", command=lambda: [test()]).grid(row=7, column=0, sticky=NW)
+Button(mainwindow, text="Ready to run", command=lambda: [test()]).grid(row=7, column=0, sticky=NW)
 mainwindow.mainloop()
