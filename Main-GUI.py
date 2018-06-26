60676c1a44ef308074ebb2ca7a9563b03bc15bc8
Nekuake - Alejandro Hurtado <ruizale55@gmail.com>
2018-06-26 23:55:28 +0200

    Code cleanup, second windows removed...

--- Main-GUI.py
+++ Main-GUI.py
@@ -11,7 +11,6 @@ try:
     status = 0
 except ImportError as e:  # Package not found, scipt shouldn't be run
     status = -1
-closed = 0
 download = 0
 upload = 0
 ping = 0
@@ -19,7 +18,7 @@ timewaited = 0
 timessofar = 0
 mainwindow = Tk()
 mainwindow.tk.call('tk', 'scaling', 2.0)
-mainwindow.geometry('550x100')
+mainwindow.geometry('550x120')
 mainwindow.configure(bg="#F0F0F0")
 mainwindow.title("Internet Speed Monitor")
 
@@ -28,15 +27,22 @@ kindoftestup = IntVar()
 kindoftestping = IntVar()
 memorypreallocation = IntVar()
 
-Checkbutton(mainwindow, text="Download", variable=kindoftestdown).grid(row=3, column=0, sticky=NW)
-Checkbutton(mainwindow, text="Upload", variable=kindoftestup).grid(row=3, column=1, sticky=NW)
-Checkbutton(mainwindow, text="Ping", variable=kindoftestping).grid(row=3, column=2, sticky=NW)
-Checkbutton(mainwindow, text="Disable Mem. preallocation.", variable=memorypreallocation).grid(row=3, column=3,
+guidownloadcheck = Checkbutton(mainwindow, text="Download", variable=kindoftestdown)
+guidownloadcheck.grid(row=3, column=0, sticky=NW)
+guiuploadcheck = Checkbutton(mainwindow, text="Upload", variable=kindoftestup).grid(row=3, column=1, sticky=NW)
+guipingcheck = Checkbutton(mainwindow, text="Ping", variable=kindoftestping).grid(row=3, column=2, sticky=NW)
+guiprealloccheck = Checkbutton(mainwindow, text="Disable Mem. preallocation.", variable=memorypreallocation).grid(row=3, column=3,
                                                                                                sticky=SW)
+Label(mainwindow, text="Nº of tests").grid(row=4, column=0, sticky=NW)
+times = Spinbox(mainwindow, width=10, from_=1, to=9999)
+times.grid(row=4, column=1, sticky=NW)
+Label(mainwindow, text="Seconds").grid(row=4, column=2, sticky=NW)
+timeout = Spinbox(mainwindow, width=10, from_=1, to=9999)
+timeout.grid(row=4, column=3, sticky=NW)
 
 
 def test():
-    global times, kindoftestping, kindoftestdown, kindoftestdown, kindoftestping, timeout, status, mainwindow
+    global
     print("Starting test function...")
     timesdef = int(times.get())
     timeoutdef = int(timeout.get())
@@ -104,37 +110,8 @@ def test():
         timewaited = 0
 
 
-def resultsgui():
-    global timesdef
-    resultwindow = Tk()
-    resultwindow.tk.call('tk', 'scaling', 2.0)
-    resultwindow.geometry('500x300')
-    resultwindow.configure(bg="#F0F0F0")
-    resultwindow.title("Internet Speed Monitor-Results")
-    globalprogressbar = Progressbar(resultwindow, orient=VERTICAL, length=100, mode='determinate', maximum=timesdef)
-    globalprogressbar.grid(row=4, column=2, sticky=SW)
-    Label(resultwindow, text="Results").grid(row=0, column=0, sticky=NW)
-    resultwindow.update()
-    downloadgui = Label(resultwindow, text="0 MB/s")
-    downloadgui.grid(row=1, column=1, sticky=NW)
-    downloadgui.configure(text="Downloadgui Speed: " + download + " KB/s.")
-    resultwindow.update()
-    uploadgui = Label(resultwindow, text="0 MB/s")
-    uploadgui.grid(row=3, column=1, sticky=NW)
-    uploadgui.configure(text="Upload Speed: " + upload + " KB/s.")
-    resultwindow.update()
-    globalprogressbar.step(1)
-    pinggui = Label(resultwindow, text="0 ms")
-    pinggui.grid(row=2, column=1, sticky=NW)
-    pinggui.configure(text="Ping: " + ping)
-    resultwindow.update()
+testthread = threading.Thread(target = test())
 
-
-Label(mainwindow, text="Nº of tests").grid(row=4, column=0, sticky=NW)
-times = Spinbox(mainwindow, width=10, from_=1, to=9999)
-times.grid(row=4, column=1, sticky=NW)
-Label(mainwindow, text="Seconds").grid(row=4, column=2, sticky=NW)
-timeout = Spinbox(mainwindow, width=10, from_=1, to=9999)
-timeout.grid(row=4, column=3, sticky=NW)
-Button(mainwindow, text="Ready to run", command=lambda: [test()]).grid(row=7, column=0, sticky=NW)
+guirun = Button(mainwindow, text="Run", command=lambda: [test()])
+guirun.grid(row=5, column=0, sticky=NW)
 mainwindow.mainloop()
