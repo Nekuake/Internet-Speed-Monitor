22498efbdcdef4e7d644332c54622a1e50b890d1
Nekuake - Alejandro Hurtado <ruizale55@gmail.com>
2018-09-18 10:37:50 +0200

    Migration from Tkinter to QT

--- Main-GUI.py
+++ Main-GUI.py
@@ -13,8 +13,8 @@ import math
 import glob
 from tkinter import ttk
 
-systemrunning=platform.system()
-print (systemrunning)
+systemrunning = platform.system()
+print(systemrunning)
 if systemrunning == "Windows":
     import winsound
 # Keys: name, sponsor, country, url
@@ -30,7 +30,6 @@ timessofar = 0
 attemps = 0
 timesdef = 0
 running = 0
-
 
 try:
     os.makedirs("Logs")
@@ -48,8 +47,7 @@ memorypreallocation = IntVar()
 deletefile = IntVar()
 infinitesting = IntVar()
 
-#Style
-
+# Style
 
 
 mainwindow.tk.call('tk', 'scaling', 2)
@@ -60,7 +58,6 @@ try:
     mainwindow.iconbitmap('internet.ico')
 except:
     print("WARNING: ICON NOT FOUND (executable?)")
-
 
 print("Creating the widgets...")
 
@@ -75,7 +72,7 @@ guitextprealloxcheck = Label(mainwindow, text="Disable Mem. preallocation.")
 guinumberoftests = Label(mainwindow, text="Nº of tests")
 status = Label(mainwindow, text="Waiting for input...")
 times = Spinbox(mainwindow, width=10, from_=1, to=9999, state='readonly')
-guitextseconds=Label(mainwindow, text="Seconds")
+guitextseconds = Label(mainwindow, text="Seconds")
 timeout = Spinbox(mainwindow, width=10, from_=1, to=9999, state='readonly')
 downloadgui = Label(mainwindow, text="0 MB/s")
 pinggui = Label(mainwindow, text="0 ms")
@@ -83,31 +80,32 @@ uploadgui = Label(mainwindow, text="0 MB/s")
 authorgui = Label(mainwindow, text="By Nekuake")
 globalprogressbar = Progressbar(mainwindow, length=100, mode='determinate', maximum=100)
 waitprogressbar = Progressbar(mainwindow, length=100, mode='determinate', maximum=100)
-guiservercountry=Label(mainwindow, text="Country")
-guiserversponsor=Label(mainwindow, text="Sponsor")
-guiservername=Label(mainwindow, text="Name")
+guiservercountry = Label(mainwindow, text="Country")
+guiserversponsor = Label(mainwindow, text="Sponsor")
+guiservername = Label(mainwindow, text="Name")
 guitextdowncheck.place(x=25, y=5)
 guitextupcheck.place(x=25, y=60)
 guitextpingcheck.place(x=25, y=180)
 guitextprealloxcheck.place(x=25, y=120)
 guidownloadcheck.place(x=10, y=5)
 guiuploadcheck.place(x=10, y=60)
-guinumberoftests.place(x=150,y=5)
+guinumberoftests.place(x=150, y=5)
 guipingcheck.place(x=10, y=180)
 guiprealloccheck.place(x=10, y=120)
 status.place(x=5, y=230)
 times.place(x=270, y=5)
-guitextseconds.place(x=168,y=60)
-timeout.place(x=270,y=60)
+guitextseconds.place(x=168, y=60)
+timeout.place(x=270, y=60)
 downloadgui.place(x=400, y=300, anchor=NE)
-pinggui.place(x=400,y=430, anchor=NE)
-uploadgui.place(x=400,y=360, anchor=NE)
-authorgui.place(x=140, y= 470)
+pinggui.place(x=400, y=430, anchor=NE)
+uploadgui.place(x=400, y=360, anchor=NE)
+authorgui.place(x=140, y=470)
 globalprogressbar.place(width=400, height=10, y=220)
 waitprogressbar.place(width=400, height=10, y=260)
 guiservername.place(x=10, y=270)
 guiservercountry.place(x=10, y=330)
 guiserversponsor.place(x=10, y=400)
+
 
 def closing():
     if running == 1:
@@ -119,14 +117,10 @@ def closing():
 mainwindow.protocol("WM_DELETE_WINDOW", closing)
 
 
-
-
-
-
 def test():
-    global guirun, mainwindow, testthread, uploadgui, downloadgui, pinggui, timesdef, globalprogressbar, status, running,\
-        globalprogressbar, infinitesting, waitprogressbar, systemrunning, testfilename, guiserversponsor,\
-        guiservercountry, guiservername, guidownloadcheck, guiuploadcheck, guinumberoftests, guipingcheck, guiprealloccheck,\
+    global guirun, mainwindow, testthread, uploadgui, downloadgui, pinggui, timesdef, globalprogressbar, status, running, \
+        globalprogressbar, infinitesting, waitprogressbar, systemrunning, testfilename, guiserversponsor, \
+        guiservercountry, guiservername, guidownloadcheck, guiuploadcheck, guinumberoftests, guipingcheck, guiprealloccheck, \
         guiinfinitesting, guitextseconds, timeout, times
     guidownloadcheck.configure(state=DISABLED)
     guiuploadcheck.configure(state=DISABLED)
@@ -152,7 +146,7 @@ def test():
     kindoftestpingdef = kindoftestping.get()
     kindoftestdowndef = kindoftestdown.get()
     kindoftestupdef = kindoftestup.get()
-    waitportion=(100/timeoutdef)
+    waitportion = (100 / timeoutdef)
     try:
         portion = (100 / (timesdef * (kindoftestupdef + kindoftestdowndef + kindoftestpingdef)))
     except ZeroDivisionError:
@@ -160,7 +154,7 @@ def test():
         guirun.configure(state=NORMAL)
         if systemrunning == "Windows":
             print("Error on Windows. Playing sound.")
-            winsound.PlaySound("SystemHand",winsound.SND_ASYNC)
+            winsound.PlaySound("SystemHand", winsound.SND_ASYNC)
         status.configure(text="INPUT ERROR. Select one kind of test")
         return
     dirname = os.path.dirname(__file__)
@@ -171,11 +165,11 @@ def test():
         if timessofar == 1:
             print("Getting best server...")
             status.configure(text="Getting best server...")
-            serverdata=s.get_best_server()
-            servercountry=serverdata["country"]
-            serversponsor=serverdata["sponsor"]
-            servername=serverdata["name"]
-            serverurl=serverdata["url"]
+            serverdata = s.get_best_server()
+            servercountry = serverdata["country"]
+            serversponsor = serverdata["sponsor"]
+            servername = serverdata["name"]
+            serverurl = serverdata["url"]
             guiservername.configure(text=servername)
             guiservercountry.configure(text=servercountry)
             guiserversponsor.configure(text=serversponsor)
@@ -209,7 +203,6 @@ def test():
                 download / 1024) + "MB/s")
             downloadgui.configure(text="Download Speed: " + str(download) + " KB/s.")
             globalprogressbar.step(portion)
-
 
         if kindoftestpingdef == 1:
             status.configure(text="Testing ping...(" + str(timessofar) + "/" + str(timesdef) + ")")
@@ -263,7 +256,7 @@ def init_thread():
 
 def disable_spinbox():
     global infinitesting, guinumberoftests, times
-    infinitcheck=infinitesting.get()
+    infinitcheck = infinitesting.get()
     if infinitcheck == 1:
         guinumberoftests.configure(state=DISABLED)
         times.configure(state=DISABLED)
@@ -273,20 +266,20 @@ def disable_spinbox():
         times.configure(state="readonly")
         print("STATE=ENABLED")
 
+
 def log_explorer():
     files = (glob.glob(os.path.dirname(__file__) + "/Logs/*.txt"))
     for x in files:
         print(x)
 
 
-
 logexplorerwindow = threading.Thread(target=log_explorer)
 logexplorerwindow.start()
 
 guirun = Button(mainwindow, text="Run", command=lambda: [init_thread(), guirun.configure(state=DISABLED)])
-guiinfinitesting = Checkbutton(mainwindow, variable=infinitesting,  command= disable_spinbox)
+guiinfinitesting = Checkbutton(mainwindow, variable=infinitesting, command=disable_spinbox)
 guitextinfinitesting = Label(mainwindow, text="Infinite")
-guirun.place(x=208 , y=180, width = 180)
+guirun.place(x=208, y=180, width=180)
 guiinfinitesting.place(x=110, y=180)
 guitextinfinitesting.place(x=125, y=180)
 
