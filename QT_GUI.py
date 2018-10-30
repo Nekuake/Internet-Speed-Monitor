fd890166ddb2e9a29db38a41e22cf828f6aa7ca8
Nekuake - Alejandro Hurtado <ruizale55@gmail.com>
2018-10-30 16:18:20 +0100

    Changes I didn't push for some reason

--- QT_GUI.py
+++ QT_GUI.py
@@ -1,5 +1,18 @@
-import sys
-from PyQt5 import QtGui
+from PySide2 import QtCore, QtGui, QtUiTools
 
-program = QtGui.QGuiApplication(sys.argv)
-mainwindow = QtGui.QWidget()
+
+def loadUiWidget(uifilename, parent=None):
+    loader = QtUiTools.QUiLoader()
+    uifile = QtCore.QFile(uifilename)
+    uifile.open(QtCore.QFile.ReadOnly)
+    ui = loader.load(uifile, parent)
+    uifile.close()
+    return ui
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = loadUiWidget(":")
+    MainWindow.show()
+    sys.exit(app.exec_())
